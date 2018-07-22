ae9e63f2259c4e604cc05cb8a82cbe7397e46ccc
Artur Arseniev <artur.catch@hotmail.it>
2018-07-22 13:35:46 +0200

    Add custom code component

--- src/components.js
+++ src/components.js
@@ -1,7 +1,93 @@
-export default (editor, config = {}) => {
-  const domc = editor.DomComponents;
-  const defaultType = domc.getType('default');
+export {
+  keyCustomCode,
+  commandCustomCode,
+  typeCustomCode,
+} from './config';
+
+export default (editor, opts = {}) => {
+  const dc = editor.DomComponents;
+  const defaultType = dc.getType('default');
   const defaultModel = defaultType.model;
-  const defaultView = defaultType.view;
-  // ...
+  const { toolbarBtnCustomCode } = opts;
+  let timedInterval;
+
+  dc.addType(typeCustomCode, {
+
+    model: defaultModel.extend({
+      defaults: {
+        ...defaultModel.prototype.defaults,
+        name: 'Custom Code',
+        editable: true,
+        ...opts.propsCustomCode,
+      },
+
+      /**
+       * Initilize the component
+       */
+      init() {
+        this.listenTo(this, `change:${keyCustomCode}`, this.onCustomCodeChange);
+        const initialCode = this.get(keyCustomCode) || opts.placeholderContent;
+        !this.components().length && this.components(initialCode);
+        const toolbar = this.get('toolbar');
+        const id = 'custom-code';
+
+        // Add the custom code toolbar button if requested and it's not already in
+        if (toolbarBtnCustomCode && !toolbar.filter(tlb => tlb.id === id ).length) {
+          toolbar.unshift({
+            id,
+            command: commandCustomCode,
+            label: `<svg viewBox="0 0 24 24">
+              <path d="M14.6 16.6l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4m-5.2 0L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4z"></path>
+            </svg>`,
+            ...toolbarBtnCustomCode
+          });
+        }
+      },
+
+      /**
+       * Callback to launch on keyCustomCode change
+       */
+      onCustomCodeChange() {
+        this.components(this.get(keyCustomCode));
+      },
+    }, {
+      /**
+       * The component can be used only if requested explicitly via `type` property
+       */
+      isComponent() {
+        return false;
+      }
+    }),
+
+    view: defaultType.view.extend({
+      events: {
+        dblclick: 'onActive',
+      },
+
+      init() {
+        this.listenTo(this.model.components(), 'add remove reset', this.onComponentsChange)
+      },
+
+      /**
+       * Things to do once inner components of custom code are changed
+       */
+      onComponentsChange() {
+        timedInterval && clearInterval(timedInterval);
+        timedInterval = setTimeout(() => {
+          const content = this.model.get(keyCustomCode);
+          console.log('onComponentsChange', content);
+
+          // Avoid rendering codes with scripts
+          if (content.indexOf('<script') >= 0) {
+            this.el.innerHTML = opts.placeholderScript;
+          }
+        }, 0);
+      },
+
+      onActive() {
+        const target = this.model;
+        this.em.get('Editor').runCommand(commandCustomCode, { target });
+      },
+    })
+  });
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import loadBlocks from './blocks';
 
 export default grapesjs.plugins.add('YOUR-PLUGIN-NAME', (editor, opts = {}) => {
   const options = { ...{
-    // default options
+    // Object to extend the default custom code properties, eg. `{ name: 'Custom Code', droppable: false, ... }`
+    propsCustomCode: {},
+
+    // Initial content of the custom code component
+    placeholderContent: '<span>Insert here your custom code</span>',
+
+    // Object to extend the default component's toolbar button for the code, eg. `{ label: '</>', attributes: { title: 'Open custom code' } }`
+    // If you pass `false` the button won't be inserted
+    toolbarBtnCustomCode: {},
+
+    // Content to show when the custom code contains `<script>`
+    placeholderScript: `<div style="pointer-events: none; padding: 10px;">
+      <svg viewBox="0 0 24 24" style="height: 30px; vertical-align: middle;">
+        <path d="M13 14h-2v-4h2m0 8h-2v-2h2M1 21h22L12 2 1 21z"></path>
+        </svg>
+      Custom code with <i>&lt;script&gt;</i> can't be rendered on the canvas
+    </div>`,
   },  ...opts };
 
   // Add components
